f936ca80db84eef264150b7e938fb90347c1aa59
aym22 <aym22@njit.edu>
2022-03-28 20:37:25 -0400

    werid glitch

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -4,8 +4,7 @@ from jinja2 import TemplateNotFound
 simple_pages = Blueprint('simple_pages', __name__,
                         template_folder='templates')
 
-
-@simple_pages.route('/')
+@simple_pages.route('/index')
 def index():
     try:
         return render_template('index.html')
