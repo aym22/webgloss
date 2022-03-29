0543775a874afc89fa4b850e88b5af72df86e4e0
aym22 <aym22@njit.edu>
2022-03-28 20:48:21 -0400

    new page

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -4,7 +4,7 @@ from jinja2 import TemplateNotFound
 simple_pages = Blueprint('simple_pages', __name__,
                         template_folder='templates')
 
-@simple_pages.route('/index')
+@simple_pages.route('/')
 def index():
     try:
         return render_template('index.html')
